069a621a8faa9f8d2ea069e5cdd2298fb9786fac
pradip patra <pradip.patra224@gmail.com>
2023-05-04 12:50:36 +0530

    Left and Right Sum Differences

---
+++ PrefixSum/Left and Right Sum Differences.java
@@ -0,0 +1,59 @@
+
+Left and Right Sum Differences
+Given a 0-indexed integer array nums, find a 0-indexed integer array answer where:
+
+answer.length == nums.length.
+answer[i] = |leftSum[i] - rightSum[i]|.
+Where:
+
+leftSum[i] is the sum of elements to the left of the index i in the array nums. If there is no such element, leftSum[i] = 0.
+rightSum[i] is the sum of elements to the right of the index i in the array nums. If there is no such element, rightSum[i] = 0.
+Return the array answer.
+
+Example 1:
+
+Input: nums = [10,4,8,3]
+Output: [15,1,11,22]
+Explanation: The array leftSum is [0,10,14,22] and the array rightSum is [15,11,3,0].
+The array answer is [|0 - 15|,|10 - 11|,|14 - 3|,|22 - 0|] = [15,1,11,22].
+Example 2:
+
+Input: nums = [1]
+Output: [0]
+Explanation: The array leftSum is [0] and the array rightSum is [0].
+The array answer is [|0 - 0|] = [0].
+ 
+
+Constraints:
+
+1 <= nums.length <= 1000
+1 <= nums[i] <= 105
+
+
+
+class Solution {
+    public int[] leftRigthDifference(int[] nums) {
+        int n = nums.length;
+        int[] leftSum = new int[n];
+        int[] rightSum = new int[n];
+        
+        // Calculate left sums
+        for (int i = 1; i < n; i++) {
+            leftSum[i] = leftSum[i-1] + nums[i-1];
+        }
+        
+        // Calculate right sums
+        for (int i = n-2; i >= 0; i--) {
+            rightSum[i] = rightSum[i+1] + nums[i+1];
+        }
+        
+        // Calculate absolute difference between left and right sums
+        int[] answer = new int[n];
+        for (int i = 0; i < n; i++) {
+            answer[i] = Math.abs(leftSum[i] - rightSum[i]);
+        }
+        
+        return answer;
+    }
+}
+
