9a1093c648cfc3589251da7935d37eb4b2f6c330
pradip patra <pradip.patra224@gmail.com>
2023-05-23 13:09:33 +0530

    Next Permutation

---
+++ Array-Medium/Next Permutation.java
@@ -0,0 +1,87 @@
+
+
+Next Permutation
+Medium
+15.2K
+4.1K
+Companies
+A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+
+For example, for arr = [1,2,3], the following are all the permutations of arr: [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
+The next permutation of an array of integers is the next lexicographically greater permutation of its integer. More formally, if all the permutations of the array are sorted in one container according to their lexicographical order, then the next permutation of that array is the permutation that follows it in the sorted container. If such arrangement is not possible, the array must be rearranged as the lowest possible order (i.e., sorted in ascending order).
+
+For example, the next permutation of arr = [1,2,3] is [1,3,2].
+Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical larger rearrangement.
+Given an array of integers nums, find the next permutation of nums.
+
+The replacement must be in place and use only constant extra memory.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [1,3,2]
+Example 2:
+
+Input: nums = [3,2,1]
+Output: [1,2,3]
+Example 3:
+
+Input: nums = [1,1,5]
+Output: [1,5,1]
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+0 <= nums[i] <= 100
+
+
+
+
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int i = n - 2;
+
+        // Find the first pair from the right where nums[i] < nums[i+1]
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+//if we dont find nums[i] < nums[i+1], then i will be -1, 
+//in this case the if wont execute
+//the whole array will be reversed using the reverse() at the end
+        if (i >= 0) {
+            int j = n - 1;
+
+            // Find the smallest element in the subarray nums[i+1:] that is greater than nums[i]
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+
+            // Swap nums[i] with nums[j]
+            swap(nums, i, j);
+        }
+
+        // Reverse the subarray nums[i+1:]
+        reverse(nums, i + 1, n - 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+}
+
