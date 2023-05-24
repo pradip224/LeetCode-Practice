554047c152a65f36d76af473424b5450c062e2fa
pradip patra <pradip.patra224@gmail.com>
2023-05-24 15:20:42 +0530

    combintion sum 2

---
+++ Array-Medium/Combination Sum II.java
@@ -0,0 +1,80 @@
+
+Combination Sum II
+Medium
+8.5K
+218
+Companies
+Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+
+Each number in candidates may only be used once in the combination.
+
+Note: The solution set must not contain duplicate combinations.
+
+ 
+
+Example 1:
+
+Input: candidates = [10,1,2,7,6,1,5], target = 8
+Output: 
+[
+[1,1,6],
+[1,2,5],
+[1,7],
+[2,6]
+]
+Example 2:
+
+Input: candidates = [2,5,2,1,2], target = 5
+Output: 
+[
+[1,2,2],
+[5]
+]
+ 
+
+Constraints:
+
+1 <= candidates.length <= 100
+1 <= candidates[i] <= 50
+1 <= target <= 30
+
+
+
+
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(candidates); // Sort the array to handle duplicates
+        backtrack(result, new ArrayList<>(), candidates, target, 0);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> combination, int[] candidates, int target, int start) {
+        if (target == 0) {
+            result.add(new ArrayList<>(combination));
+            return;
+        }
+
+        for (int i = start; i < candidates.length; i++) {
+            // Skip duplicates to avoid duplicate combinations
+            if (i > start && candidates[i] == candidates[i - 1])
+                continue;
+
+            int num = candidates[i];
+            if (num > target)
+                return;
+
+            combination.add(num);
+            backtrack(result, combination, candidates, target - num, i + 1);
+            combination.remove(combination.size() - 1);
+        }
+    }
+}
+
+
