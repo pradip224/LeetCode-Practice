88f9d4df3bce3c5a8cf905d2c5e6fa4b02b03548
pradip patra <pradip.patra224@gmail.com>
2023-05-26 13:49:22 +0530

    Group Anagrams

---
+++ Array-Medium/Group Anagrams.java
@@ -0,0 +1,61 @@
+
+
+Group Anagrams
+Medium
+15.3K
+440
+Companies
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+Example 2:
+
+Input: strs = [""]
+Output: [[""]]
+Example 3:
+
+Input: strs = ["a"]
+Output: [["a"]]
+ 
+
+Constraints:
+
+1 <= strs.length <= 104
+0 <= strs[i].length <= 100
+strs[i] consists of lowercase English letters.
+
+
+
+
+
+import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            // Sort the characters of the string to identify anagrams
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String sortedStr = new String(chars);
+
+            // Add the string to its corresponding group in the map
+            if (!map.containsKey(sortedStr)) {
+                map.put(sortedStr, new ArrayList<>());
+            }
+            map.get(sortedStr).add(str);
+        }
+
+        // Convert the values of the map to a list of lists
+        return new ArrayList<>(map.values());
+    }
+}
+
