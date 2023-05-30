730ce7b1b85ab2b7b3b9fb123d3c2055cfe9cde9
pradip patra <pradip.patra224@gmail.com>
2023-05-30 16:06:26 +0530

    set matrix zeroes

---
+++ Array-Medium/Set Matrix Zeroes.java
@@ -0,0 +1,64 @@
+
+Set Matrix Zeroes
+Medium
+11.5K
+621
+Companies
+Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+You must do it in place.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[1,0,1],[0,0,0],[1,0,1]]
+Example 2:
+
+
+Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[0].length
+1 <= m, n <= 200
+-231 <= matrix[i][j] <= 231 - 1
+
+
+
+
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        boolean[] rows = new boolean[m];
+        boolean[] columns = new boolean[n];
+        
+        // Step 1: Find positions of zeros
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    rows[i] = true;
+                    columns[j] = true;
+                }
+            }
+        }
+        
+        // Step 2: Set rows and columns to zeros
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (rows[i] || columns[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
+
