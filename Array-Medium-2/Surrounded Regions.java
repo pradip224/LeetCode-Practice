50a0809843fdd14e2fddd1443f9c1c72f2e22617
pradip patra <pradip.patra224@gmail.com>
2023-06-06 13:03:47 +0530

    surrounded regions

---
+++ Array-Medium-2/Surrounded Regions.java
@@ -0,0 +1,97 @@
+Surrounded Regions
+Medium
+7.2K
+1.5K
+Companies
+Given an m x n matrix board containing 'X' and 'O', capture all regions that are 4-directionally surrounded by 'X'.
+
+A region is captured by flipping all 'O's into 'X's in that surrounded region.
+
+ 
+
+Example 1:
+
+
+Input: board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
+Output: [["X","X","X","X"],["X","X","X","X"],["X","X","X","X"],["X","O","X","X"]]
+Explanation: Notice that an 'O' should not be flipped if:
+- It is on the border, or
+- It is adjacent to an 'O' that should not be flipped.
+The bottom 'O' is on the border, so it is not flipped.
+The other three 'O' form a surrounded region, so they are flipped.
+Example 2:
+
+Input: board = [["X"]]
+Output: [["X"]]
+ 
+
+Constraints:
+
+m == board.length
+n == board[i].length
+1 <= m, n <= 200
+board[i][j] is 'X' or 'O'.
+
+
+
+
+
+class Solution {
+    public void solve(char[][] board) {
+        if (board == null || board.length == 0) {
+            return;
+        }
+        
+        int m = board.length;
+        int n = board[0].length;
+        
+        // Traverse the first and last columns
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') {
+                dfs(board, i, 0);
+            }
+            if (board[i][n - 1] == 'O') {
+                dfs(board, i, n - 1);
+            }
+        }
+        
+        // Traverse the first and last rows
+        for (int j = 0; j < n; j++) {
+            if (board[0][j] == 'O') {
+                dfs(board, 0, j);
+            }
+            if (board[m - 1][j] == 'O') {
+                dfs(board, m - 1, j);
+            }
+        }
+        
+        // Flip 'O' to 'X' and 'T' to 'O'
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                } else if (board[i][j] == 'T') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+    
+    private void dfs(char[][] board, int row, int col) {
+        int m = board.length;
+        int n = board[0].length;
+        
+        if (row < 0 || col < 0 || row >= m || col >= n || board[row][col] != 'O') {
+            return;
+        }
+        
+        board[row][col] = 'T'; // Temporarily mark 'O' as 'T'
+        
+        // Traverse the neighbors in four directions
+        dfs(board, row - 1, col); // Up
+        dfs(board, row + 1, col); // Down
+        dfs(board, row, col - 1); // Left
+        dfs(board, row, col + 1); // Right
+    }
+}
+
