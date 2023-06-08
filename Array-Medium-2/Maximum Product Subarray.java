4671c5922e74ffe5fa882ccfb92bf5c2f1eb71ba
pradip patra <pradip.patra224@gmail.com>
2023-06-08 12:29:23 +0530

    maximum product subarray

---
+++ Array-Medium-2/Maximum Product Subarray.java
@@ -0,0 +1,52 @@
+Maximum Product Subarray
+Medium
+16.1K
+491
+Companies
+Given an integer array nums, find a 
+subarray
+ that has the largest product, and return the product.
+
+The test cases are generated so that the answer will fit in a 32-bit integer.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+Example 2:
+
+Input: nums = [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+ 
+
+Constraints:
+
+1 <= nums.length <= 2 * 104
+-10 <= nums[i] <= 10
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+
+
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        int maxProduct = nums[0];
+        int minProduct = nums[0];
+        int result = nums[0];
+        
+        for (int i = 1; i < nums.length; i++) {
+            int tempMax = Math.max(nums[i], Math.max(nums[i] * maxProduct, nums[i] * minProduct));
+            minProduct = Math.min(nums[i], Math.min(nums[i] * maxProduct, nums[i] * minProduct));
+            
+            maxProduct = tempMax;
+            result = Math.max(result, maxProduct);
+        }
+        
+        return result;
+    }
+}
+
