710b524fdfe845dcc45d2f6f884e02803dd38b01
pradip patra <pradip.patra224@gmail.com>
2023-06-19 14:59:24 +0530

    implement trie (prefix tree)

---
+++ String-Medium/Implement Trie (Prefix Tree).java
@@ -0,0 +1,81 @@
+
+Implement Trie (Prefix Tree)
+Medium
+10.3K
+114
+Companies
+A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. There are various applications of this data structure, such as autocomplete and spellchecker.
+
+Implement the Trie class:
+
+Trie() Initializes the trie object.
+void insert(String word) Inserts the string word into the trie.
+boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
+boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+ 
+
+Example 1:
+
+Input
+["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+[[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+Output
+[null, null, true, false, true, null, true]
+
+Explanation
+Trie trie = new Trie();
+trie.insert("apple");
+trie.search("apple");   // return True
+trie.search("app");     // return False
+trie.startsWith("app"); // return True
+trie.insert("app");
+trie.search("app");     // return True
+ 
+
+Constraints:
+
+1 <= word.length, prefix.length <= 2000
+word and prefix consist only of lowercase English letters.
+At most 3 * 104 calls in total will be made to insert, search, and startsWith.
+
+
+
+
+
+class Trie {
+
+    Set<String> set;
+
+    public Trie() {
+        this.set = new HashSet<>();
+        
+    }
+    
+    public void insert(String word) {
+        this.set.add(word);
+        
+    }
+    
+    public boolean search(String word) {
+        return this.set.contains(word);
+        
+    }
+    
+    public boolean startsWith(String prefix) {
+        for(String s : this.set){
+            if(s.length() >= prefix.length() && s.substring(0,prefix.length()).equals(prefix) ){
+                return true;
+            }
+        }
+        return false;        
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
+
