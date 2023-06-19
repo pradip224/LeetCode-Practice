c287646beb4b06eaf8b991d3f24d6edfa8378c0d
pradip patra <pradip.patra224@gmail.com>
2023-06-19 14:56:52 +0530

    repeated DNA sequence

---
+++ String-Medium/Repeated DNA Sequences.java
@@ -0,0 +1,58 @@
+Repeated DNA Sequences
+Medium
+2.9K
+483
+Companies
+The DNA sequence is composed of a series of nucleotides abbreviated as 'A', 'C', 'G', and 'T'.
+
+For example, "ACGAATTCCG" is a DNA sequence.
+When studying DNA, it is useful to identify repeated sequences within the DNA.
+
+Given a string s that represents a DNA sequence, return all the 10-letter-long sequences (substrings) that occur more than once in a DNA molecule. You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+Output: ["AAAAACCCCC","CCCCCAAAAA"]
+Example 2:
+
+Input: s = "AAAAAAAAAAAAA"
+Output: ["AAAAAAAAAA"]
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s[i] is either 'A', 'C', 'G', or 'T'.
+
+
+
+
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        List<String> result = new ArrayList<>();
+        Set<String> seen = new HashSet<>();
+        Set<String> repeated = new HashSet<>();
+
+        for (int i = 0; i <= s.length() - 10; i++) {
+            String sequence = s.substring(i, i + 10);
+            if (seen.contains(sequence)) {
+                repeated.add(sequence);
+            } else {
+                seen.add(sequence);
+            }
+        }
+
+        result.addAll(repeated);
+        return result;
+    }
+}
+
