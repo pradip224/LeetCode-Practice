ff881d45460763b74aa9a9b42b93d05a87f4b1df
pradip patra <pradip.patra224@gmail.com>
2023-07-04 13:03:53 +0530

    convert sorted list to binary search tree

---
+++ Medium/Convert Sorted List to Binary Search Tree.java
@@ -0,0 +1,99 @@
+
+
+Convert Sorted List to Binary Search Tree
+Medium
+6.9K
+150
+Companies
+Given the head of a singly linked list where elements are sorted in ascending order, convert it to a 
+height-balanced
+ binary search tree.
+
+ 
+
+Example 1:
+
+
+Input: head = [-10,-3,0,5,9]
+Output: [0,-3,9,-10,null,5]
+Explanation: One possible answer is [0,-3,9,-10,null,5], which represents the shown height balanced BST.
+Example 2:
+
+Input: head = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in head is in the range [0, 2 * 104].
+-105 <= Node.val <= 105
+
+
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+
+ class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        
+        if (head.next == null) {
+            return new TreeNode(head.val);
+        }
+        
+        ListNode mid = findMiddleNode(head);
+        TreeNode root = new TreeNode(mid.val);
+        
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(mid.next);
+        
+        return root;
+    }
+    
+    private ListNode findMiddleNode(ListNode head) {
+        ListNode prevPtr = null;
+        ListNode slowPtr = head;
+        ListNode fastPtr = head;
+        
+        while (fastPtr != null && fastPtr.next != null) {
+            prevPtr = slowPtr;
+            slowPtr = slowPtr.next;
+            fastPtr = fastPtr.next.next;
+        }
+        
+        if (prevPtr != null) {
+            prevPtr.next = null;
+        }
+        
+        return slowPtr;
+    }
+}
