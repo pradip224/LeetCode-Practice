68eaacd2dd1d6abf7aa1100f178014f10c30423a
pradip patra <pradip.patra224@gmail.com>
2023-07-07 20:23:46 +0530

    reorder list

---
+++ Medium-2/Reorder List.java
@@ -0,0 +1,91 @@
+
+Reorder List
+Medium
+9.1K
+301
+Companies
+You are given the head of a singly linked-list. The list can be represented as:
+
+L0 → L1 → … → Ln - 1 → Ln
+Reorder the list to be on the following form:
+
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4]
+Output: [1,4,2,3]
+Example 2:
+
+
+Input: head = [1,2,3,4,5]
+Output: [1,5,2,4,3]
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 5 * 104].
+1 <= Node.val <= 1000
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+ 
+ class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) {
+            return;
+        }
+        
+        // Step 1: Find the middle of the linked list
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        // Step 2: Split the list into two halves
+        ListNode secondHalf = slow.next;
+        slow.next = null;
+        
+        // Step 3: Reverse the second half of the linked list
+        ListNode prev = null;
+        ListNode curr = secondHalf;
+        ListNode next = null;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        secondHalf = prev;
+        
+        // Step 4: Merge the two halves of the linked list
+        ListNode p1 = head;
+        ListNode p2 = secondHalf;
+        while (p2 != null) {
+            ListNode temp1 = p1.next;
+            ListNode temp2 = p2.next;
+            p1.next = p2;
+            p2.next = temp1;
+            p1 = temp1;
+            p2 = temp2;
+        }
+    }
+}
+
