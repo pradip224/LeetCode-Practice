c3e8f4df5107f65afd8a84bdc94903ab0ed30c28
pradip patra <pradip.patra224@gmail.com>
2023-07-27 13:11:19 +0530

    max consecutive ones III

---
+++ best-75/Max Consecutive Ones III.java
@@ -0,0 +1,58 @@
+
+Max Consecutive Ones III
+Medium
+7.1K
+76
+Companies
+Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+Output: 6
+Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+Example 2:
+
+Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+Output: 10
+Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+nums[i] is either 0 or 1.
+0 <= k <= nums.length
+
+class Solution {
+    public int longestOnes(int[] nums,int k) {
+        int left = 0;
+        int right = 0;
+        int maxLen = 0;
+        int zeroCount = 0;
+
+        while (right < nums.length) {
+            if (nums[right] == 0) {
+                zeroCount++;
+            }
+
+            while (zeroCount > k) {
+                if (nums[left] == 0) {
+                    zeroCount--;
+                }
+                left++;
+            }
+
+            maxLen = Math.max(maxLen, right - left + 1);
+
+            right++;
+        }
+
+        return maxLen;
+    }
+}
+
