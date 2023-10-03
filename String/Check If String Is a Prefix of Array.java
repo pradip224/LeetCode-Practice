37644125683b220a3c2d9222aff0b9e496d3b1d0
pradip patra <pradip.patra224@gmail.com>
2023-10-03 22:07:25 +0530

    check if string is a prefix of array

---
+++ String/Check If String Is a Prefix of Array.java
@@ -0,0 +1,54 @@
+1961. Check If String Is a Prefix of Array
+Easy
+441
+80
+Companies
+Given a string s and an array of strings words, determine whether s is a prefix string of words.
+
+A string s is a prefix string of words if s can be made by concatenating the first k strings in words for some positive k no larger than words.length.
+
+Return true if s is a prefix string of words, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "iloveleetcode", words = ["i","love","leetcode","apples"]
+Output: true
+Explanation:
+s can be made by concatenating "i", "love", and "leetcode" together.
+Example 2:
+
+Input: s = "iloveleetcode", words = ["apples","i","love","leetcode"]
+Output: false
+Explanation:
+It is impossible to make s using a prefix of arr.
+ 
+
+Constraints:
+
+1 <= words.length <= 100
+1 <= words[i].length <= 20
+1 <= s.length <= 1000
+words[i] and s consist of only lowercase English letters.
+
+
+class Solution {
+    public boolean isPrefixString(String s, String[] words) {
+        StringBuilder prefix = new StringBuilder();
+        
+        for (String word : words) {
+            prefix.append(word);
+            
+            if (prefix.toString().equals(s)) {
+                return true;
+            }
+            
+            if (prefix.length() > s.length()) {
+                break;
+            }
+        }
+        
+        return false;
+    }
+}
