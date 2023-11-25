98c3cd639c718724a6a6e8d73bba8f2f569c77e7
pradip patra <pradip.patra224@gmail.com>
2023-11-25 16:30:11 +0530

    minimum string length after removing substring

---
+++ String/Minimum String Length After Removing Substrings.java
@@ -0,0 +1,57 @@
+2696. Minimum String Length After Removing Substrings
+Easy
+349
+8
+Companies
+You are given a string s consisting only of uppercase English letters.
+
+You can apply some operations to this string where, in one operation, you can remove any occurrence of one of the substrings "AB" or "CD" from s.
+
+Return the minimum possible length of the resulting string that you can obtain.
+
+Note that the string concatenates after removing the substring and could produce new "AB" or "CD" substrings.
+
+ 
+
+Example 1:
+
+Input: s = "ABFCACDB"
+Output: 2
+Explanation: We can do the following operations:
+- Remove the substring "ABFCACDB", so s = "FCACDB".
+- Remove the substring "FCACDB", so s = "FCAB".
+- Remove the substring "FCAB", so s = "FC".
+So the resulting length of the string is 2.
+It can be shown that it is the minimum length that we can obtain.
+Example 2:
+
+Input: s = "ACBBD"
+Output: 5
+Explanation: We cannot do any operations on the string so the length remains the same.
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s consists only of uppercase English letters.
+
+
+import java.util.Stack;
+
+class Solution {
+    public int minLength(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty() && (stack.peek() == 'A' && c == 'B' || stack.peek() == 'C' && c == 'D')) {
+                stack.pop(); // Remove the previous character from the stack
+            } else {
+                stack.push(c); // Add the current character to the stack
+            }
+        }
+
+        return stack.size();
+    }
+
+  
+}
