4a00cc3fbc2d4e4ffc5020c8ee41ad86dbe1ff5b
pradip patra <pradip.patra224@gmail.com>
2024-06-08 19:06:05 +0530

    find the xor of numbers which appear twice

---
+++ HashTable/Find the XOR of Numbers Which Appear Twice.java
@@ -0,0 +1,69 @@
+3158. Find the XOR of Numbers Which Appear Twice
+Solved
+Easy
+Topics
+Companies
+Hint
+You are given an array nums, where each number in the array appears either once or twice.
+
+Return the bitwise XOR of all the numbers that appear twice in the array, or 0 if no number appears twice.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,1,3]
+
+Output: 1
+
+Explanation:
+
+The only number that appears twice in nums is 1.
+
+Example 2:
+
+Input: nums = [1,2,3]
+
+Output: 0
+
+Explanation:
+
+No number appears twice in nums.
+
+Example 3:
+
+Input: nums = [1,2,2,1]
+
+Output: 3
+
+Explanation:
+
+Numbers 1 and 2 appeared twice. 1 XOR 2 == 3.
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 50
+1 <= nums[i] <= 50
+Each number in nums appears either once or twice.
+
+
+class Solution {
+    public int duplicateNumbersXOR(int[] nums) {
+        Map<Integer, Integer> count = new HashMap<>();
+        int result = 0;
+        
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+        
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            if (entry.getValue() == 2) {
+                result ^= entry.getKey();
+            }
+        }
+        
+        return result;
+    }
+}
