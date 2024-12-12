a2c6eb1f8cd75d7f7376cd89aeeb99311a84d874
Pradip Patra <pradip.patra224@gmail.com>
2024-12-12 19:41:08 +0530

    Create Circular Array Loop.java

---
+++ Array-M/Circular Array Loop.java
@@ -0,0 +1,101 @@
+457. Circular Array Loop
+Solved
+Medium
+Topics
+Companies
+You are playing a game involving a circular array of non-zero integers nums. Each nums[i] denotes the number of indices forward/backward you must move if you are located at index i:
+
+If nums[i] is positive, move nums[i] steps forward, and
+If nums[i] is negative, move nums[i] steps backward.
+Since the array is circular, you may assume that moving forward from the last element puts you on the first element, and moving backwards from the first element puts you on the last element.
+
+A cycle in the array consists of a sequence of indices seq of length k where:
+
+Following the movement rules above results in the repeating index sequence seq[0] -> seq[1] -> ... -> seq[k - 1] -> seq[0] -> ...
+Every nums[seq[j]] is either all positive or all negative.
+k > 1
+Return true if there is a cycle in nums, or false otherwise.
+
+ 
+
+Example 1:
+
+
+Input: nums = [2,-1,1,2,2]
+Output: true
+Explanation: The graph shows how the indices are connected. White nodes are jumping forward, while red is jumping backward.
+We can see the cycle 0 --> 2 --> 3 --> 0 --> ..., and all of its nodes are white (jumping in the same direction).
+Example 2:
+
+
+Input: nums = [-1,-2,-3,-4,-5,6]
+Output: false
+Explanation: The graph shows how the indices are connected. White nodes are jumping forward, while red is jumping backward.
+The only cycle is of size 1, so we return false.
+Example 3:
+
+
+Input: nums = [1,-1,5,1,4]
+Output: true
+Explanation: The graph shows how the indices are connected. White nodes are jumping forward, while red is jumping backward.
+We can see the cycle 0 --> 1 --> 0 --> ..., and while it is of size > 1, it has a node jumping forward and a node jumping backward, so it is not a cycle.
+We can see the cycle 3 --> 4 --> 3 --> ..., and all of its nodes are white (jumping in the same direction).
+ 
+
+Constraints:
+
+1 <= nums.length <= 5000
+-1000 <= nums[i] <= 1000
+nums[i] != 0
+ 
+
+
+class Solution {
+    public boolean circularArrayLoop(int[] nums) {
+        boolean[] visited = new boolean[nums.length];
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (!visited[i]) {
+                
+                int slow = i;
+                int fast = i;
+                boolean isForward = nums[i] >= 0;
+                while (true) {
+                    slow = findNextIndex(nums, isForward, slow);
+                    if (slow == -1) {
+                        break;
+                    }
+                    fast = findNextIndex(nums, isForward, fast);
+                    if (fast == -1) {
+                        break;
+                    }
+                    fast = findNextIndex(nums, isForward, fast);
+                    if (fast == -1) {
+                        break;
+                    }
+                    if (fast == slow) {
+                        return true;
+                    }
+                    visited[slow] = true;
+                    visited[fast] = true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    int findNextIndex(int[] arr, boolean isForward, int curr) {
+        boolean direction = arr[curr] >= 0;
+        if (direction != isForward) {
+            return -1;
+        }
+        int nextIndex = (arr[curr] + curr) % arr.length;
+        if (nextIndex < 0) {
+            nextIndex += arr.length;
+        }
+        if (nextIndex == curr) {
+            return -1;
+        }
+        return nextIndex;
+    }
+}
