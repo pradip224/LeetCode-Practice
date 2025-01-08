0b238e20a04917c55e71f6e49bb89d95ba34700a
Pradip Patra <pradip.patra224@gmail.com>
2025-01-08 13:13:55 +0530

    Create 01 Matrix.java

---
+++ Array-M/01 Matrix.java
@@ -0,0 +1,70 @@
+542. 01 Matrix
+Solved
+Medium
+Topics
+Companies
+Given an m x n binary matrix mat, return the distance of the nearest 0 for each cell.
+
+The distance between two cells sharing a common edge is 1.
+
+ 
+
+Example 1:
+
+
+Input: mat = [[0,0,0],[0,1,0],[0,0,0]]
+Output: [[0,0,0],[0,1,0],[0,0,0]]
+Example 2:
+
+
+Input: mat = [[0,0,0],[0,1,0],[1,1,1]]
+Output: [[0,0,0],[0,1,0],[1,2,1]]
+ 
+
+Constraints:
+
+m == mat.length
+n == mat[i].length
+1 <= m, n <= 104
+1 <= m * n <= 104
+mat[i][j] is either 0 or 1.
+There is at least one 0 in mat.
+ 
+
+
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int rows = mat.length;
+        int cols = mat[0].length;
+        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        Queue<int[]> queue = new ArrayDeque<>();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (mat[i][j] == 0) {
+                    queue.add(new int[]{i, j});
+                } else {
+                    mat[i][j] = Integer.MAX_VALUE;
+                }
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            int row = cell[0];
+            int col = cell[1];
+
+            for (int[] direction : directions) {
+                int newRow = row + direction[0];
+                int newCol = col + direction[1];
+
+                if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols && mat[newRow][newCol] > mat[row][col] + 1) {
+                    mat[newRow][newCol] = mat[row][col] + 1;
+                    queue.add(new int[]{newRow, newCol});
+                }
+            }
+        }
+
+        return mat;        
+    }
+}
