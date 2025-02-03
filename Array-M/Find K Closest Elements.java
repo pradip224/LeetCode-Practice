1ce9548d51df0be8092a7ba49f34771594c2d9e8
Pradip Patra <pradip.patra224@gmail.com>
2025-02-03 23:03:42 +0530

    Create Find K Closest Elements.java

---
+++ Array-M/Find K Closest Elements.java
@@ -0,0 +1,59 @@
+658. Find K Closest Elements
+Solved
+Medium
+Topics
+Companies
+Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array. The result should also be sorted in ascending order.
+
+An integer a is closer to x than an integer b if:
+
+|a - x| < |b - x|, or
+|a - x| == |b - x| and a < b
+ 
+
+Example 1:
+
+Input: arr = [1,2,3,4,5], k = 4, x = 3
+
+Output: [1,2,3,4]
+
+Example 2:
+
+Input: arr = [1,1,2,3,4,5], k = 4, x = -1
+
+Output: [1,1,2,3]
+
+ 
+
+Constraints:
+
+1 <= k <= arr.length
+1 <= arr.length <= 104
+arr is sorted in ascending order.
+-104 <= arr[i], x <= 104
+
+
+import java.util.*;
+
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int left = 0, right = arr.length - 1;
+
+        // Narrow down the range to k elements
+        while (right - left >= k) {
+            if (Math.abs(arr[left] - x) > Math.abs(arr[right] - x)) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        // Collect the k closest elements
+        List<Integer> result = new ArrayList<>();
+        for (int i = left; i <= right; i++) {
+            result.add(arr[i]);
+        }
+
+        return result;
+    }
+}
