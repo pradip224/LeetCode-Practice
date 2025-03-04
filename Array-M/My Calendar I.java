7624927587c6e74bdfbccda57f8e205b10fa4105
Pradip Patra <pradip.patra224@gmail.com>
2025-03-04 22:57:01 +0530

    Create My Calendar I.java

---
+++ Array-M/My Calendar I.java
@@ -0,0 +1,63 @@
+729. My Calendar I
+Solved
+Medium
+Topics
+Companies
+Hint
+You are implementing a program to use as your calendar. We can add a new event if adding the event will not cause a double booking.
+
+A double booking happens when two events have some non-empty intersection (i.e., some moment is common to both events.).
+
+The event can be represented as a pair of integers startTime and endTime that represents a booking on the half-open interval [startTime, endTime), the range of real numbers x such that startTime <= x < endTime.
+
+Implement the MyCalendar class:
+
+MyCalendar() Initializes the calendar object.
+boolean book(int startTime, int endTime) Returns true if the event can be added to the calendar successfully without causing a double booking. Otherwise, return false and do not add the event to the calendar.
+ 
+
+Example 1:
+
+Input
+["MyCalendar", "book", "book", "book"]
+[[], [10, 20], [15, 25], [20, 30]]
+Output
+[null, true, false, true]
+
+Explanation
+MyCalendar myCalendar = new MyCalendar();
+myCalendar.book(10, 20); // return True
+myCalendar.book(15, 25); // return False, It can not be booked because time 15 is already booked by another event.
+myCalendar.book(20, 30); // return True, The event can be booked, as the first event takes every time less than 20, but not including 20.
+ 
+
+Constraints:
+
+0 <= start < end <= 109
+At most 1000 calls will be made to book.
+
+
+class MyCalendar {
+    TreeMap<Integer, Integer> calcy;
+    public MyCalendar() {
+    calcy = new TreeMap();
+
+    }
+    
+    public boolean book(int start, int end) {
+    Integer prevBook = calcy.floorKey(start);
+    Integer nextBook = calcy.ceilingKey(start);
+    if((prevBook==null || calcy.get(prevBook)<=start) && (nextBook==null || end<=nextBook)) 
+    { 
+    calcy.put(start, end);
+    return true;
+    }
+    return false;
+    }
+}
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(start,end);
+ */
