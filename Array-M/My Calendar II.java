e2c0c5fc8e13c5b0593e73c861d84811a7445b83
Pradip Patra <pradip.patra224@gmail.com>
2025-03-05 23:39:57 +0530

    Create My Calendar II.java

---
+++ Array-M/My Calendar II.java
@@ -0,0 +1,99 @@
+731. My Calendar II
+Solved
+Medium
+Topics
+Companies
+Hint
+You are implementing a program to use as your calendar. We can add a new event if adding the event will not cause a triple booking.
+
+A triple booking happens when three events have some non-empty intersection (i.e., some moment is common to all the three events.).
+
+The event can be represented as a pair of integers startTime and endTime that represents a booking on the half-open interval [startTime, endTime), the range of real numbers x such that startTime <= x < endTime.
+
+Implement the MyCalendarTwo class:
+
+MyCalendarTwo() Initializes the calendar object.
+boolean book(int startTime, int endTime) Returns true if the event can be added to the calendar successfully without causing a triple booking. Otherwise, return false and do not add the event to the calendar.
+ 
+
+Example 1:
+
+Input
+["MyCalendarTwo", "book", "book", "book", "book", "book", "book"]
+[[], [10, 20], [50, 60], [10, 40], [5, 15], [5, 10], [25, 55]]
+Output
+[null, true, true, true, false, true, true]
+
+Explanation
+MyCalendarTwo myCalendarTwo = new MyCalendarTwo();
+myCalendarTwo.book(10, 20); // return True, The event can be booked. 
+myCalendarTwo.book(50, 60); // return True, The event can be booked. 
+myCalendarTwo.book(10, 40); // return True, The event can be double booked. 
+myCalendarTwo.book(5, 15);  // return False, The event cannot be booked, because it would result in a triple booking.
+myCalendarTwo.book(5, 10); // return True, The event can be booked, as it does not use time 10 which is already double booked.
+myCalendarTwo.book(25, 55); // return True, The event can be booked, as the time in [25, 40) will be double booked with the third event, the time [40, 50) will be single booked, and the time [50, 55) will be double booked with the second event.
+ 
+
+Constraints:
+
+0 <= start < end <= 109
+At most 1000 calls will be made to book.
+
+
+
+class Event
+{
+  int start;
+  int end;
+  public Event(int start, int end)
+  {
+    this.start = start;
+    this.end = end;
+  }  
+}
+
+class MyCalendarTwo {
+    List<Event> overLapBookings;
+    List<Event> bookings;
+
+    public MyCalendarTwo() {
+        overLapBookings = new ArrayList<>();
+        bookings = new ArrayList<>();
+    }
+    
+    public boolean book(int start, int end) {
+        
+    for(Event e : overLapBookings)
+    {
+      if(isOverlap(e, start, end))
+      return false;  
+    } 
+
+    for(Event e : bookings)
+    {
+      if(isOverlap(e, start, end))
+      {
+        overLapBookings.add(getOverlapEvent(e, start, end));
+      }    
+    }  
+       bookings.add(new Event(start, end));
+
+       return true; 
+    }
+
+    public boolean isOverlap(Event e, int start, int end)
+    {
+      return Math.max(e.start, start)<Math.min(e.end, end); 
+    }
+
+    public Event getOverlapEvent(Event e, int start, int end)
+    {
+      return new Event(Math.max(e.start, start), Math.min(e.end, end));  
+    }
+}
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo obj = new MyCalendarTwo();
+ * boolean param_1 = obj.book(start,end);
+ */
